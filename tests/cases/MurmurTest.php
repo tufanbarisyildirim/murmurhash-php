a01c354f9bde6a9011c9a0c0873a5d8f4e9f0343
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2020-12-04 09:19:21 +0300

    php8

--- tests/cases/MurmurTest.php
+++ tests/cases/MurmurTest.php
@@ -1,8 +1,9 @@
 <?php
 
 use lastguest\Murmur;
+use PHPUnit\Framework\TestCase;
 
-class MurmurTest extends PHPUnit_Framework_TestCase {
+class MurmurTest extends TestCase {
 
     public function testHash3_int() {
       $this->assertEquals(236294829, Murmur::hash3_int("The quick brown fox jumps over the lazy dog", 1234567), "hash3_int");
